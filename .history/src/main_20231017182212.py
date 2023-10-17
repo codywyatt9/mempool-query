473433b84eda4e5b91e8fb9c59a4d93c955fa1f8
kd9x <zetazeros@protonmail.com>
2023-10-17 18:23:53 +0100

    input node url

--- .history/src/main_20231017182212.py
+++ .history/src/main_20231017182212.py
@@ -2,7 +2,7 @@ from web3.auto import Web3
 import asyncio
 import json
 
-# input your web socket node credentials in this field.
+# enter your web socket node credentials here
 # this will allow us to stream transactions
 wss = 'wss://bold-sly-resonance.quiknode.pro/aa862642de3dcdad72ab18e5ac7c6babfaec8461/'
 web3 = Web3(Web3.WebsocketProvider(wss))
